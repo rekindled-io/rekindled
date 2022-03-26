046296940fccc55780e71e1b3dc5aebf1de78207
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-03-26 13:27:33 +1100

    configured env and ran black

--- backend/rekindled/asgi.py
+++ backend/rekindled/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rekindled.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rekindled.settings")
 
 application = get_asgi_application()
--- backend/rekindled/wsgi.py
+++ backend/rekindled/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rekindled.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rekindled.settings")
 
 application = get_wsgi_application()
