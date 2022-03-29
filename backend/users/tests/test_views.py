ace1183dca45cbfbf36355e674c9f0a064ec1d70
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-03-29 19:21:57 +1100

    new test cases for user app

--- backend/users/tests/test_views.py
+++ backend/users/tests/test_views.py
@@ -32,3 +32,16 @@ class UserViewTest(APITestCase):
         res = self.client.get(self.url)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_user_detail(self):
+        res = self.client.get(self.url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_user_when_anonymous(self):
+        res = self.client.get(self.url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_check_user_email_is_invisible_when_anonymous(self):
+        res = self.client.get(self.url)
