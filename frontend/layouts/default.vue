dbcf530f8d5c9d3908e8aceb6ac6b49e9fd4498b
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-04-15 03:07:36 +1000

    fixup default layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <LayoutHeader />
+    <div class="flex-grow bg-gray-100">
+      <nuxt />
+    </div>
+    <LayoutFooter />
+  </div>
+</template>
