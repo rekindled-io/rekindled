1c11502f1a2cec8775e6587f1c006dcf98e2e107
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-04-15 03:00:53 +1000

    add heading component

---
+++ frontend/components/Heading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-yellow-400">
+    <div class="max-w-5xl mx-auto py-8 px-4">
+      <span
+        class="bg-black text-white text-2xl font-semibold px-2 py-3 rounded"
+        >{{ content }}</span
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
