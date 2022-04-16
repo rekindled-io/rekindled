e844bda7670cf64f28a9781dce44bb801540d0a7
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-04-16 23:15:09 +1000

    media/static urls

--- backend/rekindled/urls.py
+++ backend/rekindled/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -19,3 +21,11 @@ urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
 ] + TokenURL
+
+if settings.DEBUG:
+
+    urlpatterns = (
+        urlpatterns
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
