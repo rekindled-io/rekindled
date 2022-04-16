85ff526aa8b4439e81c126d237da9d4c2a37f8b7
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-04-16 23:19:33 +1000

    handle qs updates

--- backend/handles/views.py
+++ backend/handles/views.py
@@ -6,3 +6,16 @@ from rest_framework import viewsets
 class HandleViewSet(viewsets.ModelViewSet):
     queryset = Handle.objects.all()
     serializer_class = HandleSerializer
+
+    def get_queryset(self):
+        qs = self.queryset.select_related(
+            "user",
+            "game_and_platform__game",
+            "game_and_platform__platform",
+        )
+
+        # Don't return user's own handles in results
+        if self.request.user.is_authenticated and self.action == "list":
+            qs = qs.exclude(user=self.request.user)
+
+        return qs
