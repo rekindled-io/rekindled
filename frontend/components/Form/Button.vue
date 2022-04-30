13585765cb91564c442da20ee13d991ed35eece5
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-01 07:06:08 +1000

    added button

---
+++ frontend/components/Form/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    type="submit"
+    class="flex justify-center primary-button w-full items-center leading-6 min-h-max"
+    :disabled="disableButton"
+    :class="[!disableButton ? 'cursor-pointer' : 'button-disabled']"
+  >
+    <Icon v-if="loading" name="spinner" class="animate-spin h-6 w-6 text-yellow-400" />
+    <span v-else>{{ text }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: 'Submit'
+    },
+    loading: {
+      required: false,
+      default: false
+    },
+    disabled: {
+      required: false,
+      default: true
+    },
+    icon: {
+      default: ''
+    }
+  },
+  computed: {
+    disableButton() {
+      return this.disabled || this.loading;
+    }
+  }
+});
+</script>
