e723c4edaee66f6ffa47c3cafe8ed38315541b93
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-01 07:46:01 +1000

    added input component

---
+++ frontend/components/Form/Input.vue
@@ -0,0 +1,147 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors, classes }"
+    tag="div"
+    slim
+    :rules="rules"
+    :vid="vid"
+  >
+    <div class="group relative">
+      <Icon
+        v-if="icon"
+        class="absolute h-6 w-6 inset-y-0 pointer-events-none"
+        :name="icon"
+      />
+      <input
+        v-model="innerValue"
+        :name="name"
+        :type="type"
+        :label="label"
+        :required="required"
+        class="w-full text-gray-800 bg-transparent border-b-2 px-1 py-0.5 font-bold text-sm outline-none transition duration-300 ease-in-out"
+        :class="{
+          'border-red-600 placeholder-red-600': errors[0],
+          'has-value': hasValue,
+          'focus:border-yellow-400': hover,
+          // Probably should convert below to calculated offset value
+          'pl-7': icon,
+        }"
+      />
+      <label
+        class="absolute transition-all transform duration-300 left-1 top-0.5 font-semibold pointer-events-none text-sm group-focus-within:text-xs group-focus-within:-translate-y-full group-focus-within:text-gray-800 group-focus-within:opacity-0"
+        :for="name"
+        :class="{
+          'text-gray-400': !errors[0],
+          'text-red-600': errors[0],
+          'text-xs -translate-y-full opacity-0': hasValue,
+          'pl-6': icon,
+        }"
+      >
+        {{ label }}
+        <span class="text-red-400">{{ required ? "•" : "" }}</span>
+      </label>
+      <span
+        v-if="errors[0]"
+        class="block text-red-600 text-xs absolute"
+        :class="classes"
+        >{{ errors[0] }}</span
+      >
+    </div>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+
+export default {
+  components: {
+    ValidationProvider,
+  },
+  props: {
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    rules: {
+      type: [Object, String],
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+      validator(value) {
+        return [
+          "url",
+          "text",
+          "password",
+          "tel",
+          "search",
+          "number",
+          "email",
+        ].includes(value);
+      },
+    },
+    vid: {
+      type: String,
+      default: undefined,
+    },
+    value: {
+      type: null,
+      default: "",
+    },
+    hover: {
+      type: Boolean,
+      default: false,
+    },
+    borderColor: {
+      type: String,
+      default: "border-gray-800",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+  data: () => ({
+    innerValue: "",
+  }),
+  computed: {
+    hasValue() {
+      return !!this.innerValue;
+    },
+    borderCSS() {
+      return {
+        "border-black": this.borderColor,
+      };
+    },
+  },
+  watch: {
+    innerValue(value) {
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.TextInput {
+  label {
+    margin-top: 1rem;
+  }
+
+  input.has-value ~ label,
+  input:focus ~ label {
+    @apply transition ease-in-out duration-300 -translate-y-5;
+  }
+}
+</style>
