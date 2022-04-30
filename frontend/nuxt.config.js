f7c25796d9a06dee7780e1c1005167f12f85eda2
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-01 04:38:22 +1000

    added some deps

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -4,24 +4,24 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'rekindled-frontend',
+    title: "rekindled-frontend",
     htmlAttrs: {
-      lang: 'en',
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "@/plugins/services.ts", src: "@/plugins/vee-validate.js" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -29,23 +29,29 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/svg",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.BASE_URL || "http://localhost:8000",
+    proxyHeaders: false,
+    //  credentials: true
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+
+  publicRuntimeConfig: {
+    baseURL: process.env.BASE_URL || "localhost:8000",
+  },
+};
---
+++ frontend/plugins/vee-validate.js
@@ -0,0 +1,69 @@
+import Vue from "vue";
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+  configure,
+} from "vee-validate";
+import {
+  required,
+  email,
+  min,
+  alpha_num,
+  between,
+  confirmed,
+} from "vee-validate/dist/rules";
+
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
+
+configure({
+  classes: {
+    valid: "is-valid",
+    invalid: "error",
+  },
+});
+
+extend("required", {
+  ...required,
+  message: "The {_field_} field is required.",
+});
+
+extend("alpha_num", {
+  ...alpha_num,
+  message: "The {_field_} field accepts alphanumeric characters only.",
+});
+
+extend("min", {
+  validate(value, args) {
+    return value.length >= args.length;
+  },
+  params: ["length"],
+  message: "The {_field_} field requires a minimum of {length} characters.",
+});
+
+extend("max", {
+  validate(value, args) {
+    return value.length <= args.length;
+  },
+  params: ["length"],
+  message: "The {_field_} field must be less than {length} characters.",
+});
+
+extend("minmax", {
+  validate(value, { min, max }) {
+    return value.length >= min && value.length <= max;
+  },
+  params: ["min", "max"],
+  message: "The {_field_} field must be between {min} and {max} characters.",
+});
+
+extend("email", {
+  ...email,
+  message: "A valid email is required.",
+});
+
+extend("confirmed", {
+  ...confirmed,
+  message: "The {_field_} does not match.",
+});
