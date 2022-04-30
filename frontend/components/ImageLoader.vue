55d42a4837d9e95fddec26993b5bee576e57394d
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-01 07:03:42 +1000

    image loader component

---
+++ frontend/components/ImageLoader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <img v-if="imageLoaded" :src="this.image.src" :class="imageClass" />
+    <div v-else class="h-48 w-48">
+      <Loading />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageSource: {
+      type: String,
+      required: true,
+    },
+    imageClass: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      imageLoaded: false,
+      image: new Image(),
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.fetchImage();
+    });
+  },
+  methods: {
+    fetchImage() {
+      this.image.onload = this.imageOnLoad;
+      this.imageLoaded = false;
+      this.image.src = this.imageSource;
+    },
+    imageOnLoad() {
+      this.imageLoaded = true;
+    },
+  },
+};
+</script>
