4d9aefadd64cb570fd03a2083c44e803fcaa5c21
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-01 15:24:14 +1000

    bunch o stuff

---
+++ frontend/components/Layout/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex justify-center items-center bg-[#f5f8ff]">
+    <div class="px-2 py-12 text-gray-800 border-gray-800">
+      <div class="justify-center text-center">
+        <p class="text-4xl font-bold">
+          <span class="border-b-4 border-gray-800">Rekindle</span>
+          <span class="text-">lost gaming friendships</span>
+        </p>
+        <div class="my-4">
+          Rekindled is a repository of gaming handles that enables you to
+          reconnect with old friends.
+          <p>
+            Try it out by searching for an old friend below, or
+            <Nuxt-Link class="font-semibold" to="/register">Register</Nuxt-Link>
+            an account to get started.
+          </p>
+        </div>
+        <div class="w-1/2 pt-8">
+          <form @submit.prevent="submit">
+            <FormInput icon="search" label="Search for an old friend..." />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div >>> input {
+  @apply border-black;
+  @apply text-lg;
+}
+div >>> label {
+  @apply text-gray-700;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <Tutorial />
+  <div>
+    <LayoutHero />
+    <Heading content="Most Popular" />
+    <Loading v-if="$fetchState.pending && !games.length" />
+    <section
+      v-else
+      class="grid grid-cols-2 px-4 sm:grid-cols-4 lg:grid-cols-5 gap-x-4 w-full xl:w-2/3 my-8 mx-auto"
+    >
+      <div v-for="(game, index) in games.results" :key="game.name" class="flex">
+        <CardsGame :data="game" :index="index" />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import { GameList } from "~/modules/game/Game";
 
 export default Vue.extend({
-  name: 'IndexPage',
-})
+  data() {
+    return {
+      games: {} as GameList,
+    };
+  },
+  async fetch() {
+    this.games = await this.$services.game.list();
+  },
+});
 </script>
