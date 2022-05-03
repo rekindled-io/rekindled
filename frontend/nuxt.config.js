ebac3b5c66f485883eaea39a4c8010ec3604448d
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-03 16:55:17 +1000

    adding tippy + css

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,64 +1,41 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "rekindled-frontend",
+    title: 'rekindled-frontend',
     htmlAttrs: {
-      lang: "en",
+      lang: 'en'
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    {
-      src: "~/plugins/vee-validate.js",
-      src: "~/plugins/services.ts",
-    },
-  ],
+  plugins: [{ src: '~/plugins/vee-validate.js' }, { src: '~/plugins/services.ts' }, { src: '~/plugins/tippy.js' }],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/svg",
-  ],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss', '@nuxtjs/svg'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
-  ],
+  modules: ['@nuxtjs/axios'],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL || "http://localhost:8000",
+    baseURL: process.env.BASE_URL || 'http://localhost:8000',
     proxyHeaders: false,
-    //  credentials: true
+    credentials: true
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ["vee-validate/dist/rules"],
+    transpile: ['vee-validate/dist/rules']
   },
 
   publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL || "localhost:8000",
-  },
+    baseURL: process.env.BASE_URL || 'localhost:8000'
+  }
 };
