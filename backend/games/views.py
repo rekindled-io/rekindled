2969c50333212e83bfee5b5b3fb6d6a00381f041
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-27 12:59:34 +1000

    added ranking and trending views

--- backend/games/views.py
+++ backend/games/views.py
@@ -1,6 +1,8 @@
 from rest_framework import mixins, status, viewsets, generics
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
+
 from django.db.models import Count
 
 from django_filters.rest_framework import DjangoFilterBackend
@@ -21,6 +23,27 @@ class GameViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     filterset_class = GameFilter
     filter_backends = [DjangoFilterBackend, OrderingFilter]
 
+    @action(detail=False, methods=["get"])
+    def ranking(self, request):
+        ranking = self.get_queryset().order_by("-handle_count")[:10]
+        serializer = self.get_serializer(ranking, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"])
+    def trending(self, request):
+        dt = datetime.today()
+        start_of_week = dt - timedelta(days=(dt.weekday() - 1) % 7)
+        trending = (
+            self.get_queryset()
+            .filter(gameandplatform__handle__created__gt=start_of_week)
+            .filter(handle_count__gt=0)
+            .annotate(
+                rank=Window(expression=Rank(), order_by=F("handle_count").desc())
+            )[:10]
+        )
+
+        serializer = self.get_serializer(trending, many=True)
 
 class PlatformViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Platform.objects.all()
