78dbd7139cf771fb3db64220f7a2c1c0564d8316
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-28 15:17:52 +1000

    updated to use game ranks for most popular

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,26 +3,25 @@
     <LayoutHero />
     <Heading content="Most Popular" />
     <Loading v-if="$fetchState.pending && !games.length" />
-    <section v-else class="grid grid-cols-2 px-4 sm:grid-cols-4 lg:grid-cols-5 gap-x-4 w-full xl:w-2/3 my-8 mx-auto">
-      <div v-for="(game, index) in games.results" :key="game.name" class="flex">
-        <CardsGame :data="game" :index="index" />
-      </div>
+    <section v-else class="grid w-full grid-cols-2 px-4 mx-auto my-8 sm:grid-cols-4 lg:grid-cols-5 gap-x-4 xl:w-2/3">
+      <CardsGame v-for="(game, index) in games" :data="game" :index="index" :key="game.name" />
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { GameList } from '~/modules/game/Game';
+import { Game } from '~/modules/game/Game';
 
 export default Vue.extend({
   data() {
     return {
-      games: {} as GameList
+      games: [] as Game[]
     };
   },
+
   async fetch() {
-    this.games = await this.$services.game.list();
+    this.games = await this.$services.game.ranking();
   }
 });
 </script>
