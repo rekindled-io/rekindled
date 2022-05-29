4b474d26563c9b3040c46f38664ed044a2014870
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-29 22:29:35 +1000

    card updates

--- frontend/components/Cards/Game.vue
+++ frontend/components/Cards/Game.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="relative my-4 transform hover:-translate-y-1 transition duration-300 ease-in-out">
+  <div class="relative my-4 transition duration-300 ease-in-out transform hover:-translate-y-1">
     <Nuxt-Link :to="`/search?game=${data.name}`">
-      <div class="absolute mx-1 my-1 bg-yellow-300 text-black py-1 px-2 font-semibold">#{{ index + 1 }}</div>
-      <div class="absolute bottom-0 right-0 bottom-7">
-        <span class="bg-yellow-300 text-black rounded-l-lg inline-block px-1.5 py-1.5 text-xs font-semibold"
-          >{{ data.handle_count }} Handles</span
-        >
+      <div class="absolute px-2 py-1 mx-1 my-1 font-semibold text-black bg-yellow-300">#{{ index + 1 }}</div>
+      <div class="absolute right-0 bottom-7">
+        <span class="bg-yellow-300 text-black rounded-l-lg inline-block px-1.5 py-1.5 text-xs font-semibold">
+          {{ data.handle_count }} Handles
+        </span>
       </div>
       <ImageLoader :image-source="`${$config.baseURL}${data.image}`" image-class="object-cover h-48" />
       <div class="items-center text-center">
-        <h2 class="font-bold tracking-tight text-gray-800 mx-auto overflow-hidden">
+        <h2 class="mx-auto overflow-hidden font-bold tracking-tight text-gray-800">
           {{ data.name }}
         </h2>
       </div>
@@ -18,12 +18,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropType } from 'vue';
+
+import { Game } from '~/modules/game/Game';
 
 export default Vue.extend({
   props: {
     data: {
-      type: Object,
+      type: Object as PropType<Game>,
       required: true
     },
     index: {
--- frontend/components/Cards/Handle.vue
+++ frontend/components/Cards/Handle.vue
@@ -1,100 +1,109 @@
 <template>
-  <div class="flex flex-col rounded-md bg-gray-300">
-    <div class="border-2 border-gray-600 bg-white relative -top-1 -left-1 rounded">
+  <div class="flex flex-col bg-gray-300 rounded-md">
+    <div class="relative bg-white border-2 border-gray-600 rounded -top-1 -left-1">
       <div
-        class="h-12 rounded-t-sm bg-black"
+        class="h-12 bg-black rounded-t-sm"
         style="background-size: cover; background-repeat: no-repeat; background-position: center"
         :style="{ 'background-image': `url(${$config.baseURL}${data.game_and_platform.cover})` }"
       ></div>
-      <div class="px-4 pb-4 pt-1">
+
+      <div class="px-4 pt-1 pb-4">
         <div class="flex">
           <img
             :src="$config.baseURL + data.game_and_platform.icon"
-            class="h-12 bg-white rounded-full border-4 border-gray-100 -mt-7"
+            class="h-12 bg-white border-4 border-gray-100 rounded-full -mt-7"
           />
         </div>
+
         <div class="flex justify-between items-center my-0.5">
-          <div class="space-x-1">
+          <div class="flex space-x-1">
             <Pill :name="data.game_and_platform['game_name']" />
             <Pill :name="data.game_and_platform['platform_name']" />
           </div>
-          <div>
-            <Pill :name="data.region" />
-          </div>
+          <Pill :name="data.region" />
         </div>
+
         <hr class="my-2" />
+
         <div class="flex items-center justify-between">
           <div class="z-10 overflow-hidden">
             <tippy v-if="data.name.length > 16" :content="data.name" arrow>
               <template #trigger>
-                <h3 class="text-gray-700 text-lg font-black text-gray-800">
+                <h3 class="text-lg font-black text-gray-700">
                   {{ data.name | truncate(32) }}
                 </h3>
               </template>
             </tippy>
             <div v-else>
-              <h3 class="text-gray-700 text-lg font-black text-gray-800">
+              <h3 class="text-lg font-black text-gray-700">
                 {{ data.name }}
               </h3>
             </div>
           </div>
+
           <div class="flex flex-row space-x-2">
-            <NuxtLink :to="`/user/${data.user}`">
+            <NuxtLink :to="`/user/${data.user}`" v-if="showUserLink">
               <Icon
-                class="w-6 h-6 p-1 rounded-full bg-gray-200 hover:bg-yellow-300"
+                class="w-6 h-6 p-1 bg-gray-200 rounded-full hover:bg-yellow-300"
                 name="user"
                 v-tippy="{ theme: 'tooltip' }"
                 content="view user's profile"
               />
             </NuxtLink>
-            <div class="rounded-full bg-gray-200">
-              <a @click="showModal(handle)">
-                <Icon
-                  class="w-6 h-6 p-1 rounded-full hover:bg-yellow-300"
-                  name="connect"
-                  v-tippy="{ theme: 'tooltip' }"
-                  content="connect with handle"
-                />
-              </a>
+            <div @click="showModal(data)" v-if="isAuthenticated">
+              <Icon
+                class="w-6 h-6 p-1 bg-gray-200 rounded-full hover:bg-yellow-300"
+                name="connect"
+                v-tippy="{ theme: 'tooltip' }"
+                content="connect with handle"
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
-    <ModalsConnect
-      v-if="modalKindleStatus"
-      :data="selectedItem"
-      :open="modalKindleStatus"
-      @cancel="modalKindleStatus = false"
-    />
+    <LazyModalsConnect v-if="modalState" :data="selected" @cancel="closeModal" />
   </div>
 </template>
 
 <script lang="ts">
+import Vue, { PropType } from 'vue';
 import { mapGetters } from 'vuex';
 
-import Vue from 'vue';
+import { Handle } from '~/modules/handle/Handle';
 
 export default Vue.extend({
   props: {
     data: {
-      type: Object,
+      type: Object as PropType<Handle>,
       required: true
+    },
+    showUserLink: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
+
+  data() {
+    return {
+      selected: {} as Handle,
+      modalState: false
+    };
+  },
+
   computed: {
     ...mapGetters('auth', ['isAuthenticated'])
   },
-  data() {
-    return {
-      selectedItem: null,
-      modalKindleStatus: false
-    };
-  },
+
   methods: {
-    showModal(item: any) {
-      this.selectedItem = item;
-      this.modalKindleStatus = true;
+    showModal(item: Handle) {
+      this.selected = item;
+      this.modalState = true;
+    },
+    closeModal() {
+      this.modalState = false;
+      this.selected = {} as Handle;
     }
   }
 });
