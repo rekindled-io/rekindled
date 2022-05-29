8c90994ddb2c2e2eda29d8044d5ab03964e56daa
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-29 22:32:37 +1000

    default layout updates (toast etc)

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen">
+    <LazyToast v-if="toast.message" :type="toast.type">{{ toast.message }}</LazyToast>
+
     <LayoutHeader />
     <div class="flex-grow bg-gray-100">
       <nuxt />
@@ -7,3 +9,25 @@
     <LayoutFooter />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  computed: {
+    toast() {
+      return this.$store.state.toast;
+    }
+  },
+
+  watch: {
+    $route(to, from) {
+      if (to.name != 'search') {
+        this.$store.dispatch('filters/clearAll', '');
+      }
+
+      this.$store.dispatch('toast/clear');
+    }
+  }
+});
+</script>
