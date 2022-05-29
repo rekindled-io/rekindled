94e9908ac9ddff713e09980259ddfba229e8d7ef
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-05-29 15:49:34 +1000

    added gravatar component

---
+++ frontend/components/Gravatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <img :src="gravatar_url" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    email: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 80
+    }
+  },
+
+  computed: {
+    gravatar_url() {
+      const hash = this.email.trim().toLowerCase();
+      return `https://www.gravatar.com/avatar/${hash}?s=${this.size}&d=retro`;
+    }
+  }
+});
+</script>
