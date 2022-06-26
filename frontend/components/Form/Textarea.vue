c4f2e1fd6342c806572e22ff2d48e151372e7625
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-06-26 20:02:50 +1000

    added textarea component

---
+++ frontend/components/Form/Textarea.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <label v-if="label" class="block text-sm font-bold text-gray-500" for="">{{ label }}</label>
+    <textarea
+      class="w-full px-3 py-2 text-sm font-semibold text-gray-900 placeholder-gray-500 transition duration-200 ease-in-out bg-gray-100 border-2 border-gray-100 rounded-md focus:outline-none focus:ring-yellow-400 focus:border-yellow-400 focus:bg-white"
+      v-model="innerValue"
+      type="text"
+      :placeholder="placeholder"
+      maxlength="255"
+    ></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    value: {
+      type: String
+    },
+    label: {
+      type: String,
+      required: false
+    }
+  },
+
+  data() {
+    return {
+      innerValue: ''
+    };
+  },
+
+  watch: {
+    innerValue(value: string) {
+      this.$emit('input', value);
+    }
+  },
+
+  created() {
+    if (this.value) {
+      this.innerValue = this.value;
+    }
+  }
+});
+</script>
